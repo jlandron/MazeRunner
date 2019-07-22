f033dd94980b6ce65e419fb3f0fc5fa7f77966cb
Josh Landron <jlandron@uw.edu>
2019-07-22 01:28:23 -0700

    Added gun and particle effects + raytracing for impact

---
+++ Assets/scripts/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+ [SerializeField] int hitPoints = 100;
+
+    public void takeDamage( int damage ) {
+        hitPoints -= damage;
+        if( hitPoints <= 0 ) {
+            Die( );
+        }
+
+    }
+
+    private void Die( ) {
+        Destroy( gameObject );
+    }
+}
---
+++ Assets/scripts/Weapon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    [Header("General")]
+    [SerializeField] float range = 100;
+    [SerializeField] int damage = 20;
+    [Header("External Dependencies")]
+    [SerializeField] Camera PlayerCamera;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject impactFX;
+    
+
+    // Update is called once per frame
+    void Update( ) {
+        if( Input.GetButtonDown( "Fire1" ) ) {
+            Shoot( );
+        }
+    }
+
+    private void Shoot( ) {
+        //TODO add fire effects
+        MuzzleFlash( );
+
+        ProcessRaycasting( );
+    }
+
+    private void MuzzleFlash( ) {
+        muzzleFlash.Play( );
+    }
+
+    private void ProcessRaycasting( ) {
+        RaycastHit hit;
+        if( Physics.Raycast( PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, range ) ) {
+            Debug.Log( hit.transform.name );
+
+            Instantiate( impactFX, hit.point , Quaternion.identity );
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>( );
+            if( !target ) {
+                return;
+            }
+
+            target.takeDamage( damage );
+        }
+    }
+}
