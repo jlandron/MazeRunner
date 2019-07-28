81eec388fea207b4ec824bba61bb6ed9d1c971da
Josh Landron <jlandron@uw.edu>
2019-07-27 22:51:41 -0700

    Added New level to match desired theme of game
    +more ememies
    +weapon zoom

---
+++ Assets/scripts/ZoomHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomHandler : MonoBehaviour
+{
+    [Header( "FOV" )]
+    [SerializeField] float zoomedInFOV = 50;
+    [SerializeField] float zoomedOutFOV = 60;
+
+    private Camera camera;
+
+    void Awake( ) {
+        camera = FindObjectOfType<Camera>( );
+    }
+    void Update( ) {
+        if( Input.GetButton( "Zoom" ) ) {
+            ZoomIn( );
+        }
+        else {
+            ZoomOut( );
+        }
+
+    }
+
+    private void ZoomOut( ) {
+        camera.fieldOfView = zoomedOutFOV;
+    }
+
+    void ZoomIn( ) {
+        camera.fieldOfView = zoomedInFOV;
+    }
+}
