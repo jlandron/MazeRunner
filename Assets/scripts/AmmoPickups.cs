136e50ed03f5ee332e0c7686dbc629391e55da2c
Josh Landron <jlandron@uw.edu>
2019-07-29 20:37:20 -0700

    Added basic pickup function

---
+++ Assets/scripts/AmmoPickups.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickups : MonoBehaviour
+{
+    private void OnTriggerEnter( Collider other ) {
+        if( other.gameObject.tag == "Player" ) {
+            print( "Player picked me up!" );
+            Destroy( gameObject );
+        }
+    }
+}
---
+++ Assets/scripts/BatteryPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    private void OnTriggerEnter( Collider other ) {
+        if( other.gameObject.tag == "Player" ) {
+            print( "Player picked me up!" );
+            Destroy( gameObject );
+        }
+    }
+}
